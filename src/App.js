2f77ff419927656e798dea7575cbdd38df3dd011
YevaMV <eva280988@gmail.com>
2022-02-23 23:19:55 +0400

    added more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="1" />
+      <Todo text="2" />
+      <Todo text="3" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
