c87d53896bd935721af099eae171e815bebc9993
YevaMV <eva280988@gmail.com>
2022-02-25 18:04:51 +0400

    event Props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,9 +4,15 @@ import Backdrop from './Backdrop';
 
 function Todo(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
   function deleteHandler() {
     setModalIsOpen(true);
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -15,8 +21,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCansel={closeModalHandler} />}
     </div>
   );
 }
